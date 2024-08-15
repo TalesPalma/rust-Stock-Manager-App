635e9449c99d5ed10c643ad125b81c1574c3f87d
Tales Palma <tales.palma_2022@outlook.com>
2024-08-15 20:14:03 -0300

    santo deus

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod services;
 
 use gtk::glib::clone;
-use gtk::glib::property::PropertySet;
-use gtk::{glib, Button, Orientation};
+use gtk::glib::property::{PropertyGet, PropertySet};
+use gtk::{glib, Button, Orientation, Text};
 use gtk::{prelude::*, Application, ApplicationWindow};
 use services::consume_api;
-use std::cell::Cell;
+use std::borrow::{Borrow, BorrowMut};
+use std::cell::{Cell, RefCell};
 use std::rc::Rc;
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld1";
@@ -23,7 +24,7 @@ async fn main() -> glib::ExitCode {
 
 fn build_ui(app: &Application) {
     //Vamos criar o button agora e add com o child
-    let button_increment = Button::builder()
+    let button_increment = gtk::Button::builder()
         .label("Increment")
         .margin_top(12)
         .margin_end(12)
@@ -31,6 +32,16 @@ fn build_ui(app: &Application) {
         .margin_bottom(12)
         .build();
 
+    //Criando nossa label
+    let label = gtk::Label::builder()
+        .label("0")
+        .margin_top(12)
+        .margin_end(12)
+        .margin_start(12)
+        .margin_bottom(12)
+        .build();
+
+    //Quando clico no button chama a thead glib do gtk e chama a função quê pega os dados da api.
     button_increment.connect_clicked(move |_| {
         glib::MainContext::default().spawn_local(async move {
             let service_api = consume_api::get_price_coin().await.unwrap();
@@ -44,6 +55,7 @@ fn build_ui(app: &Application) {
 
     // Inserindo os button dentro da box
     gtk_box.append(&button_increment);
+    gtk_box.append(&label);
 
     //build a janela
     let window = ApplicationWindow::builder()
